2d4d24d53f9423177e1f8171b267f733513b90df
poonam-jain <poonam.jain@mixorg.com>
2013-04-26 12:24:02 +0530

    Update php_demo.php

--- naitra/dashboard/php_demo.php
+++ naitra/dashboard/php_demo.php
@@ -13,7 +13,7 @@ if(isset($_POST['submit']))
 </head>
 
 <body>
-<form method="post" action="/naitra/dashboard/php_demo.php">
+<form method="post" action="naitra/dashboard/php_demo.php">
 	Enter name :<input type="text" name="name" />
 	<input type="submit" name="submit" value="submit" />
 </form>
