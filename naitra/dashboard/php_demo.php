822b5ee2c992f7960417d42757ae9d8af90e6e87
poonam-jain <poonam.jain@mixorg.com>
2013-04-26 12:25:36 +0530

    Update php_demo.php

--- naitra/dashboard/php_demo.php
+++ naitra/dashboard/php_demo.php
@@ -5,15 +5,14 @@ if(isset($_POST['submit']))
 	echo @$name;
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Untitled Document</title>
 </head>
 
 <body>
-<form method="post" action="naitra/dashboard/php_demo.php">
+<form method="post" action="php_demo.php">
 	Enter name :<input type="text" name="name" />
 	<input type="submit" name="submit" value="submit" />
 </form>
